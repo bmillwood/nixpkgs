081fa7bb27c1bc1586e0c9b55dded265e0d0bf72
Ben Millwood <thebenmachine+git@gmail.com>
2024-10-21 21:44:53 +0100

    nixos/nginx: HSTS includeSubdomains default false
    For more-or-less the same reasons that HSTS defaults false: accidentally
    enabling it when you didn't intend to can be really annoying.

--- nixos/modules/services/web-servers/nginx/vhost-options.nix
+++ nixos/modules/services/web-servers/nginx/vhost-options.nix
@@ -260,7 +260,7 @@ with lib;
 
       includeSubdomains = mkOption {
         type = types.bool;
-        default = true;
+        default = false;
         description = ''
           If HTTP Strict Transport Security (HSTS) is enabled by
           `strictTransportSecurity.enable`, also specify the `includeSubdomains`
